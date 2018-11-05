2023c76385f02506cdf0f556771ada2171f148f6
Tomasz Robaczewski <robak86@gmail.com>
2018-11-05 21:49:07 +0100

    simplify Omit type

--- lib/utils/types.ts
+++ lib/utils/types.ts
@@ -1,14 +1,1 @@
-/**
- * for literal unions
- * @example Sub<'Y' | 'X', 'X'> // === 'Y'
- */
-export type Sub<O extends string,
-    D extends string> = {[K in O]:(Record<D, never> & Record<string, K>)[K]}[O]
-
-/**
- * Remove the keys represented by the string union type D from the object type O.
- *
- * @example Omit<{a: number, b: string}, 'a'> // === {b: string}
- * @example Omit<{a: number, b: string}, keyof {a: number}> // === {b: string}
- */
-export type Omit<O, D extends string> = Pick<O, Sub<keyof O, D>>
+export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
