6fd959773f4047a7378c4e2c8c1796f507fc5d88
robak <robak86@gmail.com>
2020-05-23 16:33:54 +0200

    Add unit tests to function resolver

--- packages/di/src/container/proxyGetter.ts
+++ packages/di/src/container/proxyGetter.ts
@@ -2,10 +2,10 @@ import { ContainerService } from './ContainerService';
 import { ContainerCache } from './container-cache';
 import { DefinitionsSet } from '../module/DefinitionsSet';
 
-export function proxyGetter(registry: DefinitionsSet<any>, cache: ContainerCache, context) {
+export function proxyGetter(registry: DefinitionsSet<any>, cache: ContainerCache, context){
   return new Proxy({} as any, {
     get(target, property: string) {
-      return ContainerService.getChild(registry, cache, context, property); //TODO: getChild is private and it should stay private - solve this
+      return ContainerService.getChild(registry, cache, context, property);
     },
   });
 }
--- packages/di/src/resolvers/FunctionResolver.ts
+++ packages/di/src/resolvers/FunctionResolver.ts
@@ -1,10 +1,10 @@
 import { DependencyResolver } from './DependencyResolver';
 import { ContainerCache } from '../container/container-cache';
 import { curry } from '../utils/curry';
-import { GlobalSingletonResolver } from './global-singleton-resolver';
 import { ModuleRegistry } from '../module/ModuleRegistry';
 import { DefinitionsSet } from '../module/DefinitionsSet';
 import { nextId } from '../utils/fastId';
+import { proxyGetter } from '../container/proxyGetter';
 
 // TODO: not sure if this should be singleton ?
 //  or we should memoize the function by dependencySelect ?  +1
@@ -16,23 +16,43 @@ export class FunctionResolver<TRegistry extends ModuleRegistry, TReturn>
   public id: string = nextId();
   public type = FunctionResolver.type;
 
-  private readonly singletonResolver: GlobalSingletonResolver<TRegistry, TReturn>;
+  private readonly curriedFunction;
+  private readonly uncurriedFunction;
+  private readonly selectDependencies;
+  private previousDependencies: any[] = [];
 
   constructor(fn: (...args: any[]) => any, depSelect) {
-    const curried = curry(fn);
-    const select = depSelect ? depSelect : () => [];
-
-    this.singletonResolver = new GlobalSingletonResolver<any>(container => {
-      const params = select(container);
-      if (params.length === fn.length) {
-        return () => fn(...params);
-      } else {
-        return curried(...params);
-      }
-    });
+    this.uncurriedFunction = fn;
+    this.curriedFunction = curry(fn);
+    this.selectDependencies = depSelect ? depSelect : () => [];
   }
 
   build(registry: DefinitionsSet<TRegistry>, cache: ContainerCache, ctx) {
-    return this.singletonResolver.build(registry, cache, ctx);
+    // TODO: not sure if this does not trigger all getter from the whole tree !!!!
+    const currentDependencies = this.selectDependencies(proxyGetter(registry, cache, ctx));
+    const requiresRevalidation = currentDependencies.some((val, idx) => val !== this.previousDependencies[idx]);
+
+    if (requiresRevalidation) {
+      this.previousDependencies = currentDependencies;
+      const instance = this.buildFunction(currentDependencies);
+      cache.setForGlobalScope(this.id, instance);
+      return instance;
+    }
+
+    if (cache.hasInGlobalScope(this.id)) {
+      return cache.getFromGlobalScope(this.id);
+    } else {
+      let instance = this.buildFunction(currentDependencies);
+      cache.setForGlobalScope(this.id, instance);
+      return instance;
+    }
+  }
+
+  private buildFunction(params) {
+    if (params.length === this.uncurriedFunction.length) {
+      return () => this.uncurriedFunction(...params);
+    } else {
+      return this.curriedFunction(...params);
+    }
   }
 }
