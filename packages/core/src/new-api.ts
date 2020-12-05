2e718a6224913dc07237302cbe853f4c2aba6b4b
Tomasz Robaczewski <robak86@gmail.com>
2020-12-05 15:34:16 +0100

    Backup before refactoring the new api

--- packages/core/src/new-api.ts
+++ packages/core/src/new-api.ts
@@ -1,12 +1,16 @@
-import { ClassType } from './utils/ClassType';
-import { AllowedKeys } from './path';
-import { PropType } from './utils/PropType';
+import { ClassType } from "./utils/ClassType";
+import { AllowedKeys } from "./path";
+import { PropType } from "./utils/PropType";
+import { Instance } from "./resolvers/abstract/Instance";
+import { FunctionResolver } from "./resolvers/FunctionResolver";
 
 type Definition<T> = {
+  kind: 'definition';
   instance: T;
 };
 
 type ModuleRegistry<T extends Record<string, ModuleEntry>> = {
+  kind: 'module';
   registry: { [K in keyof T]: any };
 };
 
@@ -58,33 +62,23 @@ function asTupleOfLiterals<T extends string, U extends [T, ...T[]]>(tuple: U): U
 const zz = asTupleOfLiterals(['a', 'b']);
 
 export class Module<TRecord extends Record<string, ModuleEntry>> implements ModuleRegistry<TRecord> {
+  kind: 'module' = 'module';
   registry;
 
-  defineUniversal<TKey extends string, TValue, TDepsKeys extends AllowedKeys<TRecord>[]>(
+  define<TKey extends string, TValue>(
+    key: TKey,
+    value: TResolver<[], TValue>,
+  ): Module<TRecord & Record<TKey, TValue extends ModuleRegistry<any> ? TValue : Definition<TValue>>>;
+  define<TKey extends string, TValue, TDepKey extends AllowedKeys<TRecord>, TDepsKeys extends [TDepKey, ...TDepKey[]]>(
     key: TKey,
     value: TResolver<Deps<TDepsKeys, MaterializedRecord<TRecord>>, TValue>,
     keys: TDepsKeys,
-  ): Module<TRecord & Record<TKey, TValue extends ModuleRegistry<any> ? TValue : Definition<TValue>>> {
-    throw new Error('Implement me');
-  }
-
-  defineUniversal2<
-    TKey extends string,
-    TValue,
-    TDepKey extends AllowedKeys<TRecord>,
-    TDepsKeys extends [TDepKey, ...TDepKey[]]
-  >(
+  ): Module<TRecord & Record<TKey, TValue extends ModuleRegistry<any> ? TValue : Definition<TValue>>>;
+  define<TKey extends string, TValue, TDepKey extends AllowedKeys<TRecord>, TDepsKeys extends [TDepKey, ...TDepKey[]]>(
     key: TKey,
-    value: TResolver<Deps<TDepsKeys, MaterializedRecord<TRecord>>, TValue>,
-    keys: TDepsKeys,
+    value: any,
+    keys?: TDepsKeys,
   ): Module<TRecord & Record<TKey, TValue extends ModuleRegistry<any> ? TValue : Definition<TValue>>> {
-    throw new Error('Implement me');
-  }
-
-  defineUniversalDebug<TKey extends string, TValue, TDepsKeys extends AllowedKeys<TRecord>[]>(
-    key: TKey,
-    keys: TDepsKeys,
-  ): Deps<TDepsKeys, MaterializedRecord<TRecord>> {
     throw new Error('Implement me');
   }
 }
@@ -124,14 +118,53 @@ const moduleImport = <TValue extends ModuleRegistry<any>>(value: TValue): TResol
   throw new Error('implement me');
 };
 
-const m222 = m.defineUniversal('a', value(1), []).defineUniversal('b', value('string'), []);
+const m222 = m.define('a', value(1)).define('b', value('string'));
+
+const value1 = moduleImport(m222);
+const value2 = value(1);
 
 const mmm = m
-  .defineUniversal('imported', moduleImport(m222), [])
-  .defineUniversal('a', value(1), [])
-  .defineUniversal('b', value('string'), [])
-  .defineUniversal2('sdf', singleton(TestClass), ['a', 'b'])
-  .defineUniversal('sdf2', singleton(TestClassUsing), ['sdf']);
+  .define('imported', value1)
+  .define('a', value2)
+  .define('b', value('string'))
+  .define('sdf', singleton(TestClass), ['a', 'b'])
+  .define('sdf2', singleton(TestClassUsing), ['sdf']);
 
 type Mat = MaterializeModule<typeof mmm>;
 // .defineUniversal('cls', singleton(TestClass), ['a', 'b']);
+
+
+
+type FunctionResolverBuilder = {
+  <TResult>(fn: () => TResult): FunctionResolver<() => TResult>;
+  <TDep1, TResult>(fn: (d1: TDep1) => TResult): FunctionResolver<(d1: TDep1) => TResult>;
+  <TDep1, TResult>(fn: (d1: TDep1) => TResult, depSelect: [Instance<TDep1>]): FunctionResolver<() => TResult>;
+  <TDep1, TDep2, TResult>(fn: (d1: TDep1, d2: TDep2) => TResult): FunctionResolver<(d1: TDep1, d2: TDep2) => TResult>;
+  <TDep1, TDep2, TResult>(fn: (d1: TDep1, d2: TDep2) => TResult, depSelect: [Instance<TDep1>]): FunctionResolver<
+    (dep2: TDep2) => TResult
+    >;
+  <TDep1, TDep2, TResult>(
+    fn: (d1: TDep1, d2: TDep2) => TResult,
+    depSelect: [Instance<TDep1>, Instance<TDep2>],
+  ): FunctionResolver<() => TResult>;
+  // 3 args
+  <TDep1, TDep2, TDep3, TResult>(fn: (d1: TDep1, d2: TDep2, d3: TDep3) => TResult): FunctionResolver<
+    (d1: TDep1, d2: TDep2, d3: TDep3) => TResult
+    >;
+  <TDep1, TDep2, TDep3, TResult>(
+    fn: (d1: TDep1, d2: TDep2, d3: TDep3) => TResult,
+    depSelect: [Instance<TDep1>],
+  ): FunctionResolver<(dep2: TDep2, dep3: TDep3) => TResult>;
+  <TDep1, TDep2, TDep3, TResult>(
+    fn: (d1: TDep1, d2: TDep2, d3: TDep3) => TResult,
+    depSelect: [Instance<TDep1>, Instance<TDep2>],
+  ): FunctionResolver<(dep3: TDep3) => TResult>;
+  <TDep1, TDep2, TDep3, TResult>(
+    fn: (d1: TDep1, d2: TDep2, d3: TDep3) => TResult,
+    depSelect: [Instance<TDep1>, Instance<TDep2>, Instance<TDep3>],
+  ): FunctionResolver<() => TResult>;
+  <TDep1, TDep2, TDep3, TDep4, TResult>(
+    fn: (d1: TDep1, d2: TDep2, d3: TDep3, d4: TDep4) => TResult,
+    depSelect: [Instance<TDep1>, Instance<TDep2>, Instance<TDep3>, Instance<TDep4>],
+  ): FunctionResolver<() => TResult>;
+};
