a48256cda9b660b21fbde8bdee194405b6622f2c
Tomasz Robaczewski <robak86@gmail.com>
2021-01-28 19:40:52 +0100

    Add scopes to factory resolver

--- packages/core/src/resolvers/FactoryResolver.ts
+++ packages/core/src/resolvers/FactoryResolver.ts
@@ -1,30 +1,57 @@
 import { ContainerContext } from '../container/ContainerContext';
 import { ClassType } from '../utils/ClassType';
-import { Instance } from './abstract/Instance';
+import { Instance, Scope } from './abstract/Instance';
 
 export interface Factory<TReturn> {
   build(): TReturn;
 }
 
 export class FactoryResolver<TReturn, TDeps extends any[]> extends Instance<TReturn, TDeps> {
-  constructor(private klass: ClassType<Factory<TReturn>, any>) {
+  constructor(private klass: ClassType<Factory<TReturn>, any>, private scope: Scope) {
     super();
   }
 
   build(context: ContainerContext): TReturn {
-    if (context.hasInGlobalScope(this.id)) {
-      return context.getFromGlobalScope(this.id);
-    } else {
+    if (this.scope === Scope.transient) {
       const dependencies = context.getDependencies(this.id);
       const args = dependencies.map(d => d.build(context));
       const factory = new this.klass(...args);
-      const instance = factory.build();
-      context.setForGlobalScope(this.id, instance);
-      return instance;
+      return factory.build();
     }
+
+    if (this.scope === Scope.singleton) {
+      if (context.hasInGlobalScope(this.id)) {
+        return context.getFromGlobalScope(this.id);
+      } else {
+        const dependencies = context.getDependencies(this.id);
+        const args = dependencies.map(d => d.build(context));
+        const factory = new this.klass(...args);
+        const instance = factory.build();
+        context.setForGlobalScope(this.id, instance);
+        return instance;
+      }
+    }
+
+    if (this.scope === Scope.request) {
+      if (context.hasInRequestScope(this.id)) {
+        return context.getFromRequestScope(this.id);
+      } else {
+        const dependencies = context.getDependencies(this.id);
+        const args = dependencies.map(d => d.build(context));
+        const factory = new this.klass(...args);
+        const instance = factory.build();
+        context.setForRequestScope(this.id, instance);
+        return instance;
+      }
+    }
+
+    throw new Error('Wrong scope')
   }
 }
 
-export function factory<TDeps extends any[], TValue>(cls: ClassType<Factory<TValue>, TDeps>): Instance<TValue, TDeps> {
-  return new FactoryResolver(cls) as any;
+export function factory<TDeps extends any[], TValue>(
+  cls: ClassType<Factory<TValue>, TDeps>,
+  scope: Scope = Scope.singleton,
+): Instance<TValue, TDeps> {
+  return new FactoryResolver(cls, scope) as any;
 }
--- packages/core/src/resolvers/__test__/FactoryResolver.test.ts
+++ packages/core/src/resolvers/__test__/FactoryResolver.test.ts
@@ -2,7 +2,7 @@ import { factory, Factory, FactoryResolver } from '../FactoryResolver';
 import { ContainerContext } from '../../container/ContainerContext';
 import { createResolverId } from '../../utils/fastId';
 import { expectType, TypeEqual } from 'ts-expect';
-import { Instance } from '../abstract/Instance';
+import { Instance, Scope } from '../abstract/Instance';
 
 describe(`FactoryResolver`, () => {
   describe(`factory`, () => {
@@ -19,44 +19,46 @@ describe(`FactoryResolver`, () => {
     });
   });
 
-  it(`returns value produced by the factory`, async () => {
-    class DummyFactory implements Factory<any> {
-      build = jest.fn().mockReturnValue('built by factory');
-    }
-
-    const factoryResolver = new FactoryResolver(DummyFactory);
-    const context = ContainerContext.empty();
-    context.setDependencies(factoryResolver.id, []);
-    const value = factoryResolver.build(context);
-    expect(value).toEqual('built by factory');
-  });
-
-  it(`cache value returned by factory`, async () => {
-    class DummyFactory implements Factory<any> {
-      build = jest.fn().mockImplementation(createResolverId);
-    }
-
-    const factoryResolver = new FactoryResolver(DummyFactory);
-    const context = ContainerContext.empty();
-    context.setDependencies(factoryResolver.id, []);
-    expect(factoryResolver.build(context)).toEqual(factoryResolver.build(context));
-  });
-
-  it(`caches factory instance`, async () => {
-    const constructorSpy = jest.fn();
-    class DummyFactory implements Factory<any> {
-      constructor() {
-        constructorSpy();
+  describe(`singleton scope`, () => {
+    it(`returns value produced by the factory`, async () => {
+      class DummyFactory implements Factory<any> {
+        build = jest.fn().mockReturnValue('built by factory');
       }
-      build = jest.fn().mockImplementation(createResolverId);
-    }
 
-    const factoryResolver = new FactoryResolver(DummyFactory);
-    const context = ContainerContext.empty();
-    context.setDependencies(factoryResolver.id, []);
+      const factoryResolver = new FactoryResolver(DummyFactory, Scope.singleton);
+      const context = ContainerContext.empty();
+      context.setDependencies(factoryResolver.id, []);
+      const value = factoryResolver.build(context);
+      expect(value).toEqual('built by factory');
+    });
 
-    factoryResolver.build(context);
-    factoryResolver.build(context);
-    expect(constructorSpy).toBeCalledTimes(1);
+    it(`cache value returned by factory`, async () => {
+      class DummyFactory implements Factory<any> {
+        build = jest.fn().mockImplementation(createResolverId);
+      }
+
+      const factoryResolver = new FactoryResolver(DummyFactory, Scope.singleton);
+      const context = ContainerContext.empty();
+      context.setDependencies(factoryResolver.id, []);
+      expect(factoryResolver.build(context)).toEqual(factoryResolver.build(context));
+    });
+
+    it(`caches factory instance`, async () => {
+      const constructorSpy = jest.fn();
+      class DummyFactory implements Factory<any> {
+        constructor() {
+          constructorSpy();
+        }
+        build = jest.fn().mockImplementation(createResolverId);
+      }
+
+      const factoryResolver = new FactoryResolver(DummyFactory, Scope.singleton);
+      const context = ContainerContext.empty();
+      context.setDependencies(factoryResolver.id, []);
+
+      factoryResolver.build(context);
+      factoryResolver.build(context);
+      expect(constructorSpy).toBeCalledTimes(1);
+    });
   });
 });
