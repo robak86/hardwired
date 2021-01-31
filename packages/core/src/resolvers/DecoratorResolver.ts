e62a9a4f8b6ea44d1d8b5910879e45880dd3feb0
Tomasz Robaczewski <robak86@gmail.com>
2021-01-31 14:31:39 +0100

    Remove unused property

--- packages/core/src/resolvers/DecoratorResolver.ts
+++ packages/core/src/resolvers/DecoratorResolver.ts
@@ -3,7 +3,6 @@ import { ContainerContext } from '../container/ContainerContext';
 import { Thunk, unwrapThunk } from '../utils/Thunk';
 
 export class DecoratorResolver<TReturn, TDeps> extends Instance<TReturn, any> {
-  usesMaterializedModule = true;
 
   constructor(
     protected decorated: Thunk<Instance<TReturn, any>>,
--- packages/core/src/strategies/abstract/BuildStrategy.ts
+++ packages/core/src/strategies/abstract/BuildStrategy.ts
@@ -1,7 +1,6 @@
 import { Instance } from '../../resolvers/abstract/Instance';
 
 export abstract class BuildStrategy<TValue> extends Instance<TValue, []> {
-  usesMaterializedModule = true;
 
   constructor(protected buildFunction: (ctx) => TValue) {
     super();
