7f77a8e6fed2a4974de1886a954ed7952d6c1399
Tomasz Robaczewski <robak86@gmail.com>
2021-03-11 18:28:06 +0100

    Fix wrong assertion for hasKey

--- packages/core/src/collections/ImmutableMap.ts
+++ packages/core/src/collections/ImmutableMap.ts
@@ -26,7 +26,7 @@ export class ImmutableMap<D extends Record<string, any>> {
   }
 
   hasKey(key): key is keyof D {
-    return !!this.records[key]; //TODO: wont work for null|0|''|undefined values :D
+    return this.keys.includes(key);
   }
 
   reverse(): ImmutableMap<D> {
