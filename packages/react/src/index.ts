e1f6ce8b42e2b0e294ec3d4370cd89b8887bd9b7
Tomasz Robaczewski <robak86@gmail.com>
2021-06-20 17:29:48 +0200

    Remove export of unfinished concepts

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,14 +1,10 @@
 export { ContainerProvider } from './components/ContainerProvider';
-export type { ContainerProviderProps } from './components/ContainerProvider';
-export { ModulesConsumer } from './components/ModulesConsumer';
-export type { ModulesConsumerProps } from './components/ModulesConsumer';
 export { ContainerScope } from './components/ContainerScope';
+export { ModulesConsumer } from './components/ModulesConsumer';
+
+export type { ContainerProviderProps } from './components/ContainerProvider';
+export type { ModulesConsumerProps } from './components/ModulesConsumer';
 
 export { useDefinition } from './hooks/useDefinition';
 export { useModule } from './hooks/useModule';
-export { useForceRender } from './hooks/helpers/useForceRender';
-
-export { useContainer } from './context/ContainerContext';
-export { isObservable } from './abstract/IObservable';
-
-export type { IObservable } from './abstract/IObservable';
+export { useModules } from './hooks/useModules';
