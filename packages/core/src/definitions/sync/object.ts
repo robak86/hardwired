cd7fb09a69eb9bfe56d96d2877eb31dde6ed36f4
robak <robak86@gmail.com>
2021-10-14 23:21:16 +0200

    Remove support of externals from object

--- packages/core/src/definitions/sync/object.ts
+++ packages/core/src/definitions/sync/object.ts
@@ -1,14 +1,15 @@
 import { TransientStrategy } from '../../strategies/sync/TransientStrategy';
 import { InstanceDefinition } from '../abstract/InstanceDefinition';
 import { v4 } from 'uuid';
-import { pickExternals, PickExternalsFromRecord } from '../../utils/PickExternals';
+import { pickExternals } from '../../utils/PickExternals';
 
-export const object = <T extends Record<keyof any, InstanceDefinition<any, any>>>(
+export const object = <T extends Record<keyof any, InstanceDefinition<any, []>>>(
   record: T,
 ): InstanceDefinition<
   { [K in keyof T]: T[K] extends InstanceDefinition<infer TInstance, any> ? TInstance : unknown },
   []
-  //PickExternalsFromRecord<T> TODO
+  // due to typescript limitations there is no way to correctly infer externals from record.
+  // Order of the record properties is not guaranteed therefore the order of externals in tuple cannot be guaranteed
 > => {
   const definitions = Object.values(record);
   const firstStrategy = definitions[0]?.strategy;
