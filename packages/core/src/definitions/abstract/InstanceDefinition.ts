ae598ec641fd3a45a5b3dea926169e54605693ce
Tomasz Robaczewski <robak86@gmail.com>
2021-10-16 16:45:04 +0200

    Add lifetime type generic type in InstanceDefinition

--- packages/core/src/definitions/abstract/InstanceDefinition.ts
+++ packages/core/src/definitions/abstract/InstanceDefinition.ts
@@ -7,14 +7,14 @@ export type InstanceDefinitionContext = ContainerContext;
 
 export type InstanceDefinition<TInstance, TLifeTime extends LifeTime, TExternals = []> = {
   id: string;
-  strategy: TLifeTime; // TODO: rename to lifeTime
+  strategy: TLifeTime;
   resolution: Resolution.sync;
   externals: Array<InstanceDefinition<any, any>>;
   create: (context: InstanceDefinitionContext, _?: TExternals) => TInstance; // _ is fake parameter introduced in order to preserve TExternal type
 };
 
 export const instanceDefinition = {
-  isAsync(val: AnyInstanceDefinition<any, any, any>): val is AsyncInstanceDefinition<any,any, any> {
+  isAsync(val: AnyInstanceDefinition<any, any, any>): val is AsyncInstanceDefinition<any, any, any> {
     return (val as any).isAsync;
   },
   isSync(val: AnyInstanceDefinition<any, any, any>): val is InstanceDefinition<any, any> {
