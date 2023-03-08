091fd7777354d8733d39f9494f5eb320fdadb1d1
Tomasz Robaczewski <robak86@gmail.com>
2023-03-08 17:28:19 +0100

    Remove stale files

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  projects: ['<rootDir>/packages/core/jest.config.js', '<rootDir>/packages/react/jest.config.js'],
-};
-
-export default config;
--- packages/core/jest.config.js
+++
@@ -1,6 +0,0 @@
-import defaults from '../../jest.shared.js';
-
-export default {
-  ...defaults,
-  displayName: 'core',
-};
