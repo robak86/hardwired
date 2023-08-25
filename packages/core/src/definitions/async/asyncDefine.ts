13180cecfa0c4bcfcc77e0e08148a2b1cb25bccc
Tomasz Robaczewski <robak86@gmail.com>
2023-08-25 17:05:29 +0200

    Renaming

--- packages/core/src/definitions/async/asyncDefine.ts
+++ packages/core/src/definitions/async/asyncDefine.ts
@@ -1,12 +1,12 @@
 import { LifeTime } from '../abstract/LifeTime.js';
 import { asyncDefinition, AsyncInstanceDefinition } from '../abstract/async/AsyncInstanceDefinition.js';
 import { ContainerContext } from '../../context/ContainerContext.js';
-import { IContainerScopes, ISyncContainer } from '../../container/IContainer.js';
+import { IContainerScopes, InstanceCreationAware } from '../../container/IContainer.js';
 import { Container } from '../../container/Container.js';
 
 export const asyncDefine = <TLifeTime extends LifeTime>(strategy: TLifeTime) => {
   return <TValue>(
-    fn: (locator: ISyncContainer<TLifeTime> & IContainerScopes<TLifeTime>) => Promise<TValue>,
+    fn: (locator: InstanceCreationAware<TLifeTime> & IContainerScopes<TLifeTime>) => Promise<TValue>,
   ): AsyncInstanceDefinition<TValue, TLifeTime> => {
     return asyncDefinition({
       strategy,
--- packages/core/src/definitions/sync/define.ts
+++ packages/core/src/definitions/sync/define.ts
@@ -1,12 +1,12 @@
 import { InstanceDefinition } from '../abstract/sync/InstanceDefinition.js';
 import { LifeTime } from '../abstract/LifeTime.js';
 import { ContainerContext } from '../../context/ContainerContext.js';
-import { IContainerScopes, ISyncContainer } from '../../container/IContainer.js';
+import { IContainerScopes, InstanceCreationAware } from '../../container/IContainer.js';
 import { Container } from '../../container/Container.js';
 
 export const define = <TLifeTime extends LifeTime>(lifetime: TLifeTime) => {
   return <TValue>(
-    buildFn: (locator: ISyncContainer<TLifeTime> & IContainerScopes<TLifeTime>) => TValue,
+    buildFn: (locator: InstanceCreationAware<TLifeTime> & IContainerScopes<TLifeTime>) => TValue,
   ): InstanceDefinition<TValue, TLifeTime> => {
     return InstanceDefinition.create(lifetime, (context: ContainerContext) => {
       return buildFn(new Container(context));
