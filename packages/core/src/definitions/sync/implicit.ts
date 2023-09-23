dcc93c9df81558425b44bc92d642faf33f434e6f
Tomasz Robaczewski <robak86@gmail.com>
2023-09-23 14:20:24 +0200

    feat: add optional meta to implicit definition

--- packages/core/src/definitions/sync/implicit.ts
+++ packages/core/src/definitions/sync/implicit.ts
@@ -2,12 +2,16 @@ import { InstanceDefinition } from '../abstract/sync/InstanceDefinition.js';
 import { LifeTime } from '../abstract/LifeTime.js';
 import { asyncDefinition, AsyncInstanceDefinition } from '../abstract/async/AsyncInstanceDefinition.js';
 
-export function implicit<T>(name: string): InstanceDefinition<T, LifeTime.scoped> {
-  return InstanceDefinition.create(LifeTime.scoped, () => {
-    throw new Error(
-      `Cannot instantiate implicit definition "${name}". Definition should be provided at the runtime, by creating new scope`,
-    );
-  });
+export function implicit<T>(name: string, meta?: object): InstanceDefinition<T, LifeTime.scoped> {
+  return InstanceDefinition.create(
+    LifeTime.scoped,
+    () => {
+      throw new Error(
+        `Cannot instantiate implicit definition "${name}". Definition should be provided at the runtime, by creating new scope`,
+      );
+    },
+    meta,
+  );
 }
 
 export function implicitAsync<T>(name: string): AsyncInstanceDefinition<T, LifeTime.scoped> {
