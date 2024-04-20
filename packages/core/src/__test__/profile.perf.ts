17d0a6b334fb952f71ef0822bcf74ea47f9ea43a
Tomasz Robaczewski <robak86@gmail.com>
2024-04-20 13:27:38 +0200

    Update perf check script

--- packages/core/src/__test__/profile.perf.ts
+++ packages/core/src/__test__/profile.perf.ts
@@ -1,11 +1,19 @@
-import now from 'performance-now';
 import { Container, container } from '../container/Container.js';
 
 import { singleton, transient } from '../definitions/definitions.js';
 import { InstanceDefinition } from '../definitions/abstract/sync/InstanceDefinition.js';
 import 'source-map-support/register';
+import Bench from 'tinybench';
+import { EagerDefinitionsInterceptor } from '../context/EagerDefinitionsInterceptor.js';
 
-function buildSingletonsTree(times: number, depth: number, currentDepth = 0): InstanceDefinition<number, any>[] {
+const eagerInterceptor = new EagerDefinitionsInterceptor();
+
+function buildSingletonTree(
+  times: number,
+  depth: number,
+  markEager: boolean,
+  currentDepth = 0,
+): InstanceDefinition<number, any>[] {
   if (currentDepth > depth) {
     return [];
   }
@@ -13,7 +21,20 @@ function buildSingletonsTree(times: number, depth: number, currentDepth = 0): In
   const definitions: any[] = [];
 
   for (let i = 0; i < times; i++) {
-    definitions.push(singleton.fn((...args: any[]) => args, ...buildSingletonsTree(times, depth, (currentDepth += 1))));
+    if (markEager && currentDepth === depth) {
+      definitions.push(
+        singleton //
+          .using(...buildSingletonTree(times, depth, markEager, (currentDepth += 1)))
+          .annotate(eagerInterceptor.eager)
+          .fn((...args: any[]) => args),
+      );
+    } else {
+      definitions.push(
+        singleton //
+          .using(...buildSingletonTree(times, depth, markEager, (currentDepth += 1)))
+          .fn((...args: any[]) => args),
+      );
+    }
   }
 
   return definitions;
@@ -27,47 +48,52 @@ function buildTransient(times: number, depth: number, currentDepth = 0): Instanc
   const definitions: any[] = [];
 
   for (let i = 0; i < times; i++) {
-    definitions.push(transient.fn((...args: any[]) => args, ...buildTransient(times, depth, (currentDepth += 1))));
+    definitions.push(
+      transient //
+        .using(...buildTransient(times, depth, (currentDepth += 1)))
+        .fn((...args: any[]) => args),
+    );
   }
 
   return definitions;
 }
 
-function resolveByObject(container: Container, def: InstanceDefinition<any, any>, times: number) {
-  const result = {
-    avg: -1,
-    max: -1,
-    min: Infinity,
-  };
-
-  const items: number[] = [];
-  let i = 0;
-
-  for (i = 0; i < times; i++) {
-    const start = now();
-    const obj = container.get(def);
-
-    const end = now();
-    const total = end - start;
-
-    if (total < result.min) {
-      result.min = total;
-    }
-    if (total > result.max) {
-      result.max = total;
-    }
-
-    items.push(total);
-  }
-
-  result.avg = items.reduce((p, c) => p + c, 0) / items.length;
-
-  return result;
-}
-
-const singletonD: any = singleton.fn((...args) => args, ...buildSingletonsTree(4, 10));
+const singletonD: any = singleton.fn((...args) => args, ...buildSingletonTree(4, 10, false));
 const transientD: any = transient.fn((...args) => args, ...buildTransient(3, 10));
+const singletonWithEagerLeafD: any = singleton.fn((...args) => args, ...buildSingletonTree(4, 10, true));
 
-const cWithoutProxy = container();
-const result10KWithoutProxy = resolveByObject(cWithoutProxy, singletonD, 1000000);
-console.log(result10KWithoutProxy);
+let cnt: Container;
+let eagerCnt: Container;
+
+const bench = new Bench({
+  time: 100,
+  setup: () => {
+    cnt = container();
+    eagerCnt = container({ interceptor: eagerInterceptor });
+  },
+  teardown: () => {
+    cnt = container();
+    eagerCnt = container({ interceptor: eagerInterceptor });
+  },
+});
+
+bench
+  .add('no interceptor: sync singleton', () => {
+    cnt.get(singletonD);
+  })
+  .add('no interceptor: sync transient', () => {
+    cnt.get(transientD);
+  })
+  .add('eager: sync singleton', () => {
+    eagerCnt.get(singletonD);
+  })
+  .add('eager: sync transient', () => {
+    eagerCnt.get(transientD);
+  })
+  .add('eager with eager leafs: sync singleton', () => {
+    eagerCnt.get(singletonWithEagerLeafD);
+  });
+
+await bench.warmup();
+await bench.run();
+console.table(bench.table());
