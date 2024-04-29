71e6f5f34c830f9e5819a0cc332da4de78b24cfe
Tomasz Robaczewski <robak86@gmail.com>
2024-04-29 12:36:16 +0200

    Add tests for container initializer

---
+++ packages/react/src/components/__test__/ContainerInitializer.test.tsx
@@ -0,0 +1,59 @@
+import { scoped } from 'hardwired';
+import { ContainerProvider } from '../ContainerProvider.js';
+import { ContainerScope } from '../ContainerScope.js';
+import { ContainerInitializer } from '../ContainerInitializer.js';
+import { useDefinition } from '../../hooks/useDefinition.js';
+import { render } from '@testing-library/react';
+import { expect } from 'vitest';
+
+describe(`ContainerInitializer`, () => {
+  function setup() {
+    class NeedToBeInitialized {
+      value = 0;
+
+      init() {
+        this.value = Math.random();
+      }
+    }
+
+    const needToBeInitialized = scoped.class(NeedToBeInitialized);
+    const initializer = scoped.thunk(c => {
+      c.use(needToBeInitialized).init();
+    });
+
+    const ValueRenderer = ({ testId }: { testId: any }) => {
+      const value = useDefinition(needToBeInitialized);
+
+      return <div data-testid={testId}>{value.value}</div>;
+    };
+
+    const TestSubject = () => (
+      <ContainerProvider>
+        <ContainerScope>
+          <ContainerInitializer init={initializer}>
+            <ValueRenderer testId={'scope1'} />
+          </ContainerInitializer>
+        </ContainerScope>
+        <ContainerScope>
+          <ContainerInitializer init={initializer}>
+            <ValueRenderer testId={'scope2'} />
+          </ContainerInitializer>
+        </ContainerScope>
+      </ContainerProvider>
+    );
+
+    return { TestSubject };
+  }
+
+  it(`runs initializers`, async () => {
+    const { TestSubject } = setup();
+    const result = render(<TestSubject />);
+    expect(result.getByTestId('scope1').textContent).toEqual('0');
+    expect(result.getByTestId('scope2').textContent).toEqual('0');
+    result.rerender(<TestSubject />);
+
+    expect(result.getByTestId('scope1').textContent).not.toEqual('0');
+    expect(result.getByTestId('scope2').textContent).not.toEqual('0');
+    expect(result.getByTestId('scope1').textContent).not.toEqual(result.getByTestId('scope2').textContent);
+  });
+});
