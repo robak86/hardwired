6894bb2c7bd6734eed35fba17e46ea611f52e962
Tomasz Robaczewski <robak86@gmail.com>
2025-03-13 19:02:40 +0100

    Bump test libraries

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    workspace: ['./packages/core/vitest.config.ts', './packages/react/vitest.config.ts'],
     coverage: {
       provider: 'v8',
       reporter: 'cobertura',
