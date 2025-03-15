11b6555506cc08192925a7c8580749ad0746c236
Tomasz Robaczewski <robak86@gmail.com>
2025-03-15 15:55:58 +0100

    Add real use case test for the integration with vitest + cleanup

--- packages/core/src/container/__test__/RealUseCases.test.ts
+++ packages/core/src/container/__test__/RealUseCases.test.ts
@@ -1,8 +1,67 @@
+import { test } from 'vitest';
+
 import { cls } from '../../definitions/cls.js';
 import { container } from '../Container.js';
 import { value } from '../../definitions/sync/value.js';
 import { unbound } from '../../definitions/sync/unbound.js';
 import { configureScope } from '../../configuration/ScopeConfiguration.js';
+import { fn } from '../../definitions/definitions.js';
+import type { IContainer } from '../IContainer.js';
+import {
+  type AsyncContainerConfigureFn,
+  configureContainer,
+  type ContainerConfigureFn,
+} from '../../configuration/ContainerConfiguration.js';
+
+describe(`Testing`, () => {
+  describe(`using container in vitest context with cleaning up resources`, () => {
+    const status = {
+      isDestroyed: false,
+    };
+
+    const dbConnection = fn.scoped(() => {
+      return {
+        destroy() {
+          status.isDestroyed = true;
+        },
+      };
+    });
+
+    const withContainer = <TConfigureFns extends Array<AsyncContainerConfigureFn | ContainerConfigureFn>>(
+      ...containerConfigFns: TConfigureFns
+    ) => {
+      return test.extend<{ use: IContainer }>({
+        use: async ({}, use: any) => {
+          const scope = await container.new(...containerConfigFns);
+
+          await use(scope);
+
+          scope.useExisting(dbConnection)?.destroy();
+        },
+      });
+    };
+
+    const setupDB = configureContainer(c => {
+      c.cascade(dbConnection);
+
+      // TODO: ideally disposal should be registered here
+    });
+
+    const it = withContainer(setupDB);
+
+    it(`uses container`, async ({ use }) => {
+      const scope = use.scope(_c => {
+        // configure
+      });
+
+      scope.use(dbConnection);
+    });
+
+    it(`has cleaned resources from the previous run`, async () => {
+      expect(status.isDestroyed).toBe(true);
+    });
+  });
+});
 
 describe(`Logger`, () => {
   describe(`branding logger with an id for a request`, () => {
