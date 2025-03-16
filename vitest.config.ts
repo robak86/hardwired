9c489f6743b04292a64bda4bcc214e0d24d3bcab
Tomasz Robaczewski <robak86@gmail.com>
2025-03-16 19:16:12 +0100

    Remove unused code

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,21 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    poolOptions: {
-      forks: {
-        execArgv: ['--expose-gc'],
-      },
-    },
     workspace: ['./packages/core/vitest.config.ts', './packages/react/vitest.config.ts'],
     coverage: {
       provider: 'v8',
       reporter: 'cobertura',
       include: ['./packages/**/*'],
-      exclude: [
-        './packages/core/src/__test__/profile.perf.ts',
-        './packages/core/src/__test__/memLeaksCheck.ts',
-        './packages/core/src/__test__/excessively-deep-instantiation-check.ts',
-      ],
+      exclude: ['./packages/core/src/__test__/profile.perf.ts', './packages/core/src/__test__/memLeaksCheck.ts'],
     },
   },
 });
