9469449c274977e7ab76b92d887c58ca22ff3452
Tomasz Robaczewski <robak86@gmail.com>
2025-03-16 12:59:05 +0100

    Remove comments

--- packages/core/src/container/CompositeDisposable.ts
+++ packages/core/src/container/CompositeDisposable.ts
@@ -1,25 +1,16 @@
 export class CompositeDisposable implements Disposable {
   private _disposables: Disposable[] = [];
 
-  constructor() {}
-
   get count() {
     return this._disposables.length;
   }
 
   registerDisposable(disposable: Disposable) {
-    // if (this._isDisposed) {
-    //   throw new Error('ContainerDisposer is disposed');
-    // }
-
     this._disposables.push(disposable);
   }
 
   [Symbol.dispose]() {
     this._disposables.forEach(disposable => disposable[Symbol.dispose]());
-
-    // this._isDisposed = true;
-    // console.log('ContainerDisposer', this._disposables);
     this._disposables.length = 0;
   }
 }
