d079f04a09811031995547bfc9d78be6d22461af
Tomasz Robaczewski <robak86@gmail.com>
2025-03-16 19:14:49 +0100

    Include only ./packages/ directories for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: 'cobertura',
+      include: ['./packages/**/*'],
       exclude: [
         './packages/core/src/__test__/profile.perf.ts',
         './packages/core/src/__test__/memLeaksCheck.ts',
