e455c0f1e5858503466d832faef23b01cceb0fbd
Tomasz Robaczewski <robak86@gmail.com>
2025-03-17 13:31:53 +0100

    Add optional debug name for unbound definitions

--- packages/core/src/definitions/unbound.ts
+++ packages/core/src/definitions/unbound.ts
@@ -1,21 +1,32 @@
 import { LifeTime } from './abstract/LifeTime.js';
 import { Definition } from './impl/Definition.js';
 
-function createUnboundDefinition(): never {
-  throw new Error(
-    `Cannot instantiate unbound definition. Definition should be provided at the runtime by creating a new scope`,
-  );
-}
-
-export class UnboundDefinition<TInstance, TLifeTime extends LifeTime> extends Definition<TInstance, TLifeTime, []> {
-  readonly kind = 'unbound';
-
-  constructor(id: symbol, strategy: TLifeTime) {
-    super(id, strategy, createUnboundDefinition);
+function executeUnboundDefinition(debugName?: string): never {
+  if (debugName) {
+    throw new Error(
+      `Cannot instantiate unbound definition "${debugName}". Definition should be provided at the runtime by creating a new scope`,
+    );
+  } else {
+    throw new Error(
+      `Cannot instantiate unbound definition. Definition should be provided at the runtime by creating a new scope`,
+    );
   }
 }
 
-// TODO: maybe this should be transient by default!!
-export function unbound<T>(): UnboundDefinition<T, LifeTime.scoped> {
-  return new UnboundDefinition<T, LifeTime.scoped>(Symbol(), LifeTime.scoped);
+export class UnboundDefinition<TInstance, TLifeTime extends LifeTime> extends Definition<TInstance, TLifeTime, []> {
+  constructor(
+    id: symbol,
+    strategy: TLifeTime,
+    private _debugName?: string,
+  ) {
+    super(id, strategy, executeUnboundDefinition.bind(null, _debugName));
+  }
+
+  get name() {
+    return this._debugName ?? super.name;
+  }
+}
+
+export function unbound<T>(debugName?: string): UnboundDefinition<T, LifeTime.scoped> {
+  return new UnboundDefinition<T, LifeTime.scoped>(Symbol(), LifeTime.scoped, debugName);
 }
