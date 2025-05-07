256217d74096ffebf042b2e3dcc22664e02bd502
Tomasz Robaczewski <robak86@gmail.com>
2025-05-07 15:57:01 +0200

    Remove merge map helper

--- packages/core/src/definitions/impl/TransientDefinition.ts
+++ packages/core/src/definitions/impl/TransientDefinition.ts
@@ -1,24 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
 import type { IContainer, NextValue } from '../../container/IContainer.js';
 import { LifeTime } from '../abstract/LifeTime.js';
 import { isThenable } from '../../utils/IsThenable.js';
-import type { HasPromiseReturningFn } from '../../utils/HasPromiseMember.js';
-import type { AwaitedPropertiesFactories } from '../abstract/InstanceDefinition.js';
 
 import { Definition } from './Definition.js';
-
-export type MergeMapResult<
-  TInstance,
-  TArgs extends unknown[],
-  TPropertyFns extends Record<PropertyKey, (use: IContainer<LifeTime.transient>, value: Awaited<TInstance>) => any>,
-> =
-  HasPromiseReturningFn<TPropertyFns[keyof TPropertyFns]> extends true
-    ? TransientDefinition<Promise<Awaited<TInstance> & AwaitedPropertiesFactories<TPropertyFns>>, TArgs>
-    : TransientDefinition<TInstance & AwaitedPropertiesFactories<TPropertyFns>, TArgs>;
 
 export class TransientDefinition<TInstance, TArgs extends unknown[]> extends Definition<
   TInstance,
@@ -47,37 +31,6 @@ export class TransientDefinition<TInstance, TArgs extends unknown[]> extends Def
         }
       },
     );
-  }
-
-  mergeMap<TProperties extends Record<string, (use: IContainer<LifeTime.transient>, value: Awaited<TInstance>) => any>>(
-    map: TProperties, //extends object ? TProperties : never,
-  ): MergeMapResult<TInstance, TArgs, TProperties> {
-    return new TransientDefinition<MergeMapResult<TInstance, TArgs, TProperties>, TArgs>(
-      this.id,
-      (ctx, ...args): MergeMapResult<TInstance, TArgs, TProperties> => {
-        const result = this.create(ctx, ...args);
-
-        if (isThenable(result)) {
-          return result.then(async instance => {
-            const mapped = {} as any;
-
-            for (const [key, fn] of Object.entries(map)) {
-              mapped[key] = await fn(ctx, instance as any);
-            }
-
-            return { ...(instance as object), ...mapped };
-          }) as any;
-        } else {
-          const mapped = {} as any;
-
-          for (const [key, fn] of Object.entries(map)) {
-            mapped[key] = fn(ctx, result);
-          }
-
-          return { ...result, ...mapped } as MergeMapResult<TInstance, TArgs, TProperties>;
-        }
-      },
-    ) as MergeMapResult<TInstance, TArgs, TProperties>;
   }
 
   flatMap<TNext>(
