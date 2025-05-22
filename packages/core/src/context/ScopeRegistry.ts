28ab5669f6192e3a1555883f355643ec750319a8
Tomasz Robaczewski <robak86@gmail.com>
2025-05-22 17:47:28 +0200

    Add support for linked BindingsRegistry

--- packages/core/src/context/ScopeRegistry.ts
+++ packages/core/src/context/ScopeRegistry.ts
@@ -2,23 +2,27 @@ import { COWMap } from './COWMap.js';
 
 export class ScopeRegistry<V> {
   static create<V>(): ScopeRegistry<V> {
-    return new ScopeRegistry<V>(COWMap.create<V>());
+    return new ScopeRegistry<V>(false, COWMap.create<V>());
   }
 
   private _overrides = new Map<symbol, V>();
 
-  constructor(private _registrations: COWMap<V>) {}
+  constructor(
+    private _isFrozen: boolean,
+    private _registrations: COWMap<V>,
+    private _prev?: ScopeRegistry<V>,
+  ) {}
 
-  hasRegistration(definitionId: symbol): boolean {
-    return this._registrations.has(definitionId);
+  findRegistration(definitionId: symbol): V | undefined {
+    return this._registrations.get(definitionId) ?? this._prev?.findRegistration(definitionId);
   }
 
-  hasOverride(definitionId: symbol): boolean {
-    return this._overrides.has(definitionId);
+  findOverride(definitionId: symbol): V | undefined {
+    return this._overrides.get(definitionId) ?? this._prev?.findOverride(definitionId);
   }
 
   find(definitionId: symbol): V | undefined {
-    return this._overrides.get(definitionId) ?? this._registrations.get(definitionId);
+    return this._overrides.get(definitionId) ?? this._registrations.get(definitionId) ?? this._prev?.find(definitionId);
   }
 
   get(definitionId: symbol): V {
@@ -26,26 +30,47 @@ export class ScopeRegistry<V> {
   }
 
   getForOverride(definitionId: symbol): V {
-    if (this._overrides.has(definitionId)) {
-      return this._overrides.get(definitionId)!;
-    }
+    const def = this.findOverride(definitionId) || this.findRegistration(definitionId);
+    // if (this._overrides.has(definitionId)) {
 
-    if (this._registrations.has(definitionId)) {
-      return this._registrations.get(definitionId)!;
-    }
-
-    throw new Error(`No definition registered for ${definitionId.toString()}.
+    if (!def) {
+      throw new Error(`No definition registered for ${definitionId.toString()}.
      If you want to modify definition make sure it's registered first using .add(...) method.`);
+    }
+
+    return def;
   }
 
   protected getRegistered(definitionId: symbol): V {
     const definition = this._registrations.get(definitionId);
 
-    if (!definition) {
-      throw new Error(`No definition registered for ${definitionId.toString()}`);
+    if (definition) {
+      return definition;
+    } else {
+      const parentDefinition = this._prev?.getRegistered(definitionId);
+
+      if (parentDefinition) {
+        return parentDefinition;
+      } else {
+        throw new Error(`No definition registered for ${definitionId.toString()}`);
+      }
+    }
+  }
+
+  forceRegister(definitionId: symbol, instance: V) {
+    if (this._isFrozen) {
+      throw new Error(`Cannot register instance with id ${definitionId.toString()} in frozen ScopeRegistry.`);
     }
 
-    return definition;
+    this._registrations.set(definitionId, instance);
+  }
+
+  forceOverride(definitionId: symbol, instance: V) {
+    if (this._isFrozen) {
+      throw new Error(`Cannot override instance with id ${definitionId.toString()} in frozen ScopeRegistry.`);
+    }
+
+    this._overrides.set(definitionId, instance);
   }
 
   register(definitionId: symbol, instance: V) {
@@ -53,7 +78,7 @@ export class ScopeRegistry<V> {
       throw new Error(`Instance with id ${definitionId.toString()} already registered. Try using .modify() instead.`);
     }
 
-    this._registrations.set(definitionId, instance);
+    this.forceRegister(definitionId, instance);
   }
 
   override(definitionId: symbol, instance: V) {
@@ -61,10 +86,14 @@ export class ScopeRegistry<V> {
       throw new Error(`Instance with id ${definitionId.toString()} not registered. Try using .register() instead.`);
     }
 
-    this._overrides.set(definitionId, instance);
+    this.forceOverride(definitionId, instance);
   }
 
   checkoutForScope() {
-    return new ScopeRegistry(this._registrations.clone());
+    return new ScopeRegistry(false, this._registrations.clone());
+  }
+
+  attach(prev: ScopeRegistry<V>) {
+    return new ScopeRegistry(true, this._registrations, prev);
   }
 }
